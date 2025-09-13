ee0536c2521ed10ff3b157a78effb37699b1f539
Ahad Khan <ahadkhan.ai306@gmail.com>
2025-09-13 13:24:18 +0530

    Login user and validation

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -7,7 +8,7 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-
+app.use(cookieParser());
 //cors configurations
 app.use(
   cors({
@@ -20,7 +21,7 @@ app.use(
 
 //import the routes
 import healthCheckRouter from "./routes/healthcheck.routes.js";
-import authRouter from "./routes/auth.routes.js"
+import authRouter from "./routes/auth.routes.js";
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
 app.use("/api/v1/auth", authRouter);
